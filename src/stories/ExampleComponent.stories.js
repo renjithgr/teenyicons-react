1b2ef750d3255477c4c657168c678c28b7c8491f
Renjith <grenjithr@gmail.com>
2021-01-29 13:41:04 -0600

    Adding first story

---
+++ src/stories/ExampleComponent.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { ThreeSixty } from '../index';
+
+export default {
+  title: 'TeenyIcons/ThreeSixty',
+  component: ThreeSixty
+};
+
+const Template = (args) => <ThreeSixty {...args} />;
+
+export const Default = Template.bind({});
