064d3f95ef860b6264466f2203354f9e77a9a2d8
Renjith <grenjithr@gmail.com>
2021-01-29 16:26:47 -0600

    Refactor storybook file

---
+++ src/stories/Teenicons.stories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { ThreeSixty } from '../index';
+import { ABTesting } from '../index';
+
+export default { title: 'Teenyicons' };
+
+export const ThreeSixtyIcon = () => <ThreeSixty />;
+ThreeSixtyIcon.storyName = "360˚";
+
+export const ABTestingIcon = () => <ABTesting />;
+ABTestingIcon.storyName = "AB Testing";
